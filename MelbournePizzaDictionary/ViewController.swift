4b057ba1a80f614bec9ce3cca78521bc84206fee
Jin Hu <jinhu@Jingle.local>
2015-05-04 03:03:18 +1000

    Initial Commit

---
+++ MelbournePizzaDictionary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MelbournePizzaDictionary
+//
+//  Created by Jin Hu on 4/05/2015.
+//  Copyright (c) 2015 Jin Hu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MelbournePizzaDictionaryTests/MelbournePizzaDictionaryTests.swift
@@ -0,0 +1,36 @@
+//
+//  MelbournePizzaDictionaryTests.swift
+//  MelbournePizzaDictionaryTests
+//
+//  Created by Jin Hu on 4/05/2015.
+//  Copyright (c) 2015 Jin Hu. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class MelbournePizzaDictionaryTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
